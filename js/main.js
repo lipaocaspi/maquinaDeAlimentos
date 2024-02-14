b5804e4826a96924c80b61f9cfe737ac9c423015
lipaocaspi <66758443+lipaocaspi@users.noreply.github.com>
2024-02-14 08:47:58 -0500

    feat: :tada: Inicio del ejercicio
    El ejercicio trata de una dispensadora de maquina de alimentos
    
    @CampusLands

---
+++ js/main.js
@@ -0,0 +1,4 @@
+// Este problema apareció en el certamen recuperativo 1 del segundo semestre de 2011 en el campus Vitacura.
+// Una máquina de alimentos tiene productos de tres tipos, A, B y C, que valen respectivamente $270, $340 y $390. La máquina acepta y da de vuelto monedas de $10, $50 y $100.
+// Escriba un programa que pida al usuario elegir el producto y luego le pida ingresar las monedas hasta alcanzar el monto a pagar. Si el monto ingresado es mayor que el precio del producto, el programa debe entregar las monedas de vuelto, una por una.
+console.log("Hello world!");
