15f15b198aa51e52eff11bb945b3adda6b3e2616
lipaocaspi <66758443+lipaocaspi@users.noreply.github.com>
2024-02-14 20:27:56 -0500

    feat: :sparkles: Creación de función
    Creación de la función encargada de realizar el cálculo
    
    @CampusLands

--- js/main.js
+++ js/main.js
@@ -6,13 +6,35 @@ let productos = ["A", "B", "C"];
 let precios = ["270", "340", "390"];
 let monedas = ["10", "50", "100"];
 
-let opcion
+let opcion;
+
+function realizarCompra (precio) {
+    confirm(precio);
+}
 
 do {
-    opcion = prompt(`Seleccione una opción:
+    opcion = prompt(`DISPENSADORA DE ALIMENTOS
+        Seleccione un producto:
         1. ${productos[0]} - $${precios[0]}
         2. ${productos[1]} - $${precios[1]}
         3. ${productos[2]} - $${precios[2]}
         4. Salir`);
+
+        switch (opcion) {
+            case '1':
+                realizarCompra(Number(precios[0]))
+                break;
+            case '2':
+                realizarCompra(Number(precios[1]))
+                break;
+            case '3':
+                realizarCompra(Number(precios[2]))
+                break;
+            case '4':
+                confirm("Gracias por usar nuestro servicio");
+                break;
+            default:
+                confirm("Opción no válida. Intente de nuevo.");
+        }
 
 } while (opcion !== '4')
